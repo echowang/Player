14263ba831804c0d662ea053b46144c103ed2eac
王亭 <tingw@hwtc.com.cn>
2018-01-22 11:40:23 +0800

    修改音乐播放页面UI

--- app/src/main/java/com/danny/player/ui/fragment/MusicPlayFragment.java
+++ app/src/main/java/com/danny/player/ui/fragment/MusicPlayFragment.java
@@ -1,13 +1,20 @@
 package com.danny.player.ui.fragment;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
+import com.danny.media.library.broadcast.AudioVolumeBroadcast;
 import com.danny.media.library.provider.MediaProviderFactory;
 import com.danny.media.library.provider.MusicProvider;
 import com.danny.media.library.model.Lrc;
@@ -33,7 +40,7 @@ import me.wcy.lrcview.LrcView;
 /**
  * Created by tingw on 2018/1/17.
  */
-public class MusicPlayFragment extends BaseFragment implements PlayerService.IServiceUIRefreshListener<Song>, MusicPlayControllerBar.MusicPlayControllerBarListener {
+public class MusicPlayFragment extends BaseFragment implements PlayerService.IServiceUIRefreshListener<Song>, MusicPlayControllerBar.MusicPlayControllerBarListener,AudioVolumeBroadcast.OnAudioVolumeChangeListener {
     private final static String TAG = MusicPlayFragment.class.getSimpleName();
 
     @BindView(R.id.music_play_container_bg)
@@ -47,11 +54,17 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
     private MusicAlbumCoverView albumCoverView;
     private LrcView mLrcViewSingle;
 
+    private SeekBar volumeSeekBar;
+    private LrcView mLrcViewFull;
+
+    private AudioManager audioManager;
+    private AudioVolumeBroadcast audioVolumeBroadcast;
+
     private PlayerService<Song> playerService;
 
     @Override
     protected int getLayout() {
-        return R.layout.fragment_music_info;
+        return R.layout.fragment_music_play;
     }
 
     @Override
@@ -61,9 +74,16 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
 
         initViewPager();
         initViewEvent();
+        initVolumeBar();
 
         playerService = PlayerApplication.getApplication().getMusicPlayerService();
         playerService.setUIRefreshListener(this);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        audioVolumeBroadcast = AudioVolumeBroadcast.registerAudioVolumeBroadcast(getContext(),this);
     }
 
     @Override
@@ -76,25 +96,68 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
         musicPlayControllerBar.updateModelStatue(playerService.getPlayerModel());
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        AudioVolumeBroadcast.unRegisterAudioVolumeBroadcast(getContext(),audioVolumeBroadcast);
+    }
+
     private void initViewPager(){
         PlayerGlide.loadLocalBlurImage(getContext(),R.mipmap.play_default_bg,4,3,containerBgView);
-
-        View coverView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_play_page_cover, null);
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View coverView = layoutInflater.inflate(R.layout.fragment_play_page_cover, null);
+        View lrcView = layoutInflater.inflate(R.layout.fragment_play_page_lrc,null);
         albumCoverView = coverView.findViewById(R.id.album_cover_view);
         mLrcViewSingle = coverView.findViewById(R.id.lrc_view_single);
-        mViewPagerContent = new ArrayList<>(1);
+        volumeSeekBar = lrcView.findViewById(R.id.sb_volume);
+        mLrcViewFull = lrcView.findViewById(R.id.lrc_view_full);
+
+        mViewPagerContent = new ArrayList<>(2);
         mViewPagerContent.add(coverView);
+        mViewPagerContent.add(lrcView);
 
         musicPlayViewPager.initIndicator();
-        musicPlayViewPager.getIndicator().setOrientation(UltraViewPager.Orientation.HORIZONTAL)
+        musicPlayViewPager.getIndicator()
+                .setOrientation(UltraViewPager.Orientation.HORIZONTAL)
                 .setFocusColor(Color.GREEN)
                 .setNormalColor(Color.WHITE)
-                .setRadius((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()));
+                .setRadius((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()))
+                .setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+        musicPlayViewPager.getIndicator()
+                .build();
         musicPlayViewPager.setAdapter(new PlayPagerAdapter(mViewPagerContent));
     }
 
     private void initViewEvent(){
         musicPlayControllerBar.setPlayControllerBarListener(this);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                LogUtil.i(TAG,"volume : " + seekBar.getProgress());
+                if (audioManager != null){
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,seekBar.getProgress(),AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+                }
+            }
+        });
+    }
+
+    private void initVolumeBar(){
+        audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+//        LogUtil.i(TAG,"volume max : " + audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+//        LogUtil.i(TAG,"volume : " + audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+
+        volumeSeekBar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        volumeSeekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
     }
 
     private void updateMusicInfo(Song song){
@@ -112,6 +175,7 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
                 if (lrcFile.exists()){
                     LogUtil.i(TAG,"lrc path : " + lrcFile.getPath());
                     mLrcViewSingle.loadLrc(lrcFile);
+                    mLrcViewFull.loadLrc(lrcFile);
                 }
             }
         }
@@ -191,6 +255,9 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
         if (mLrcViewSingle.hasLrc()){
             mLrcViewSingle.updateTime(progress);
         }
+        if (mLrcViewFull.hasLrc()){
+            mLrcViewFull.updateTime(progress);
+        }
     }
 
     @Override
@@ -201,5 +268,12 @@ public class MusicPlayFragment extends BaseFragment implements PlayerService.ISe
     @Override
     public void onMusicChange(Song song) {
         updateMusicInfo(song);
+    }
+
+    @Override
+    public void onChange(int volume) {
+        if (volume >= 0){
+            volumeSeekBar.setProgress(volume);
+        }
     }
 }
